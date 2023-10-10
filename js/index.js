874c13a0d01a2065a0f049de29f942b6f4c97c64
Jenny Berggreen <143997461+jenny-berggreen@users.noreply.github.com>
2023-10-10 13:41:29 +0200

    Declare image description strings

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/* ---------------------- description strings ------------------------ */
+let image1_description = "Image 1 description";
+let image2_description = "Image 2 description";
+let image3_description = "Image 3 description";
+let image4_description = "Image 4 description";
+let image5_description = "Image 5 description";
