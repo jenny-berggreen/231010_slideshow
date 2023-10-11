66b86467842101274ce60724b64a622439bbf820
Jenny Berggreen <143997461+jenny-berggreen@users.noreply.github.com>
2023-10-11 10:26:08 +0200

    Declare strings array and needed functions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,37 @@
 /* ---------------------- description strings ------------------------ */
-let image1_description = "Image 1 description";
-let image2_description = "Image 2 description";
-let image3_description = "Image 3 description";
-let image4_description = "Image 4 description";
-let image5_description = "Image 5 description";
+const descriptionStrings = 
+["Image 1 description", 
+"Image 2 description", 
+"Image 3 description",
+"Image 4 description",
+"Image 5 description"]
+
+/* ----------------------  ------------------------ */
+
+let slideIndex = 0;
+
+showSlide(slideIndex);
+
+function changeIndex(n) {
+	showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+	let slides = document.querySelectorAll(".image");
+	console.log(slides.length); // 5
+	let description = document.querySelector(".image-description");
+
+	// if previous button is pressed at slide 1, slideIndex should be 5
+	if (slideIndex < 0) {
+		slideIndex === slides.length - 1;
+	}
+
+	// if next button is pressed at slide 5, slideIndex should be 1
+	if (slideIndex > slides.length - 1) {
+		slideIndex === 0;
+	}
+
+	slides[slideIndex].classList.add("display-block");
+	description.innerText = descriptionStrings[slideIndex];
+
+}
